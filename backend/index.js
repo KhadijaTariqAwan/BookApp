08ee3c4f660afd8b5e1ed0923b0022625eb03c7e
Khadija Tariq Awan <khadijatariq559@example.com>
2025-07-03 19:43:02 +0500

    class 16

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -7,6 +8,22 @@ app.use(cors())
 app.use(express.json())
 
 const port = 4000
+
+mongoose.connect('mongodb+srv://khadijatariqawan57:khadija123%23@cluster0.qgkogde.mongodb.net/BookApp?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+const db = mongoose.connection
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error', err)
+})
+
+db.once('open', () => {
+  console.log('Connected to MongoDB sucessfully')
+})
 
 app.get('/', (req, res) => {
   res.send('Hi There!')
